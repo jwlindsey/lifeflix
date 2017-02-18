e4b4d28fef5b56d8a395db17d94a96d82aaf9179
james <j.w.lindsey1986@gmail.com>
2017-02-18 21:40:02 +0000

    typo fixes

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,6 +1,6 @@
 class Instructor::CoursesController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course
+  before_action :require_authorized_for_current_course, except: [:new, :create]
 
   def new
     @course = Course.new
