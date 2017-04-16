4b3c177cb01ffbc2eb7327c444e2fc1b98308566
james <j.w.lindsey1986@gmail.com>
2017-04-16 03:15:22 +0000

    fixed sections order issue

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -9,6 +9,11 @@ class Instructor::SectionsController < ApplicationController
     redirect_to instructor_course_path(current_course)
   end
 
+  def update
+    current_section.update_attributes(section_params)
+    render text: 'updated!'
+  end
+
   private
   
   def require_authorized_for_current_course
@@ -22,7 +27,17 @@ class Instructor::SectionsController < ApplicationController
     @current_course ||= Course.find(params[:course_id])
   end
 
+  def require_authorized_for_current_section
+    if current_section.course.user !=current_user
+      return render text: 'Unauthorized', status: unauthorized
+    end
+  end
+
   def section_params
-    params.require(:section).permit(:title)
+    params.require(:section).permit(:title, :row_order_position)
+  end
+  
+  def current_section
+    @current_section ||= Section.find(params[:id])
   end
 end
